e7477dde42e3ab36368dc9897ec121c38b86844b
Jairo Matías <jairo_1829@hotmail.com>
2026-08-18 07:45:40 -0400

    feat(auth): integrar rutas de cuentas y verificación

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,8 @@ import {
   Routes,
 } from "react-router-dom";
 
-import Navbar from "./components/layout/Navbar";
-import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/navbar";
+import Footer from "./components/layout/footer";
 
 import Home from "./pages/Home";
 import Actores from "./pages/Actores";
@@ -15,6 +15,10 @@ import FormularioPelicula from "./pages/FormularioPelicula";
 import RepartoPelicula from "./pages/RepartoPelicula";
 import PerfilActor from "./pages/PerfilActor";
 import PerfilPelicula from "./pages/PerfilPelicula";
+import Login from "./pages/Login";
+import Registro from "./pages/Registro";
+import ReclamarPerfil from "./pages/ReclamarPerfil";
+import AdminVerificaciones from "./pages/AdminVerificaciones";
 
 function App() {
   return (
@@ -25,41 +29,22 @@ function App() {
         <main className="container py-4 app-main">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/registro" element={<Registro />} />
 
             <Route path="/actores" element={<Actores />} />
-            <Route
-              path="/actores/nuevo"
-              element={<FormularioActor />}
-            />
-            <Route
-              path="/actores/editar/:id"
-              element={<FormularioActor />}
-            />
-            <Route
-              path="/actores/:id"
-              element={<PerfilActor />}
-            />
+            <Route path="/actores/nuevo" element={<FormularioActor />} />
+            <Route path="/actores/editar/:id" element={<FormularioActor />} />
+            <Route path="/actores/:id" element={<PerfilActor />} />
+            <Route path="/actores/:actorId/reclamar" element={<ReclamarPerfil />} />
 
-            <Route
-              path="/peliculas"
-              element={<Peliculas />}
-            />
-            <Route
-              path="/peliculas/nueva"
-              element={<FormularioPelicula />}
-            />
-            <Route
-              path="/peliculas/editar/:id"
-              element={<FormularioPelicula />}
-            />
-            <Route
-              path="/peliculas/:id"
-              element={<PerfilPelicula />}
-            />
-            <Route
-              path="/peliculas/:id/reparto"
-              element={<RepartoPelicula />}
-            />
+            <Route path="/peliculas" element={<Peliculas />} />
+            <Route path="/peliculas/nueva" element={<FormularioPelicula />} />
+            <Route path="/peliculas/editar/:id" element={<FormularioPelicula />} />
+            <Route path="/peliculas/:id" element={<PerfilPelicula />} />
+            <Route path="/peliculas/:id/reparto" element={<RepartoPelicula />} />
+
+            <Route path="/admin/verificaciones" element={<AdminVerificaciones />} />
           </Routes>
         </main>
 
