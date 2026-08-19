b45b2c3ccf16511444e82fffe89a104db451a5da
Jairo Matías <jairo_1829@hotmail.com>
2026-08-18 20:19:51 -0400

    style: cargar estilos cinematograficos publicos

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,7 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 // Debe cargarse después de Bootstrap para aplicar los estilos de CineRD.
 import "./index.css";
 import "./styles/portal.css";
+import "./styles/cinematic-public.css";
 
 import App from "./App.jsx";
 
