5297426dfedfb5b970b519d7751dc2df3931bdd3
Jairo Matías <jairo_1829@hotmail.com>
2026-08-19 05:38:49 -0400

    feat: exponer gestión de talentos verificados

--- backend/src/routes/verificacionRoutes.js
+++ backend/src/routes/verificacionRoutes.js
@@ -4,7 +4,9 @@ const {
   misSolicitudes,
   miPerfilTalento,
   listarPendientes,
+  listarVinculacionesActivas,
   revisarSolicitud,
+  revocarVinculacion,
 } = require("../controllers/verificacionController");
 const { autenticar, autorizar } = require("../middlewares/auth");
 
@@ -14,6 +16,8 @@ router.post("/actor/:actorId", autenticar, autorizar("USUARIO"), crearSolicitud)
 router.get("/mis-solicitudes", autenticar, misSolicitudes);
 router.get("/mi-perfil", autenticar, autorizar("TALENTO_VERIFICADO"), miPerfilTalento);
 router.get("/admin/pendientes", autenticar, autorizar("ADMINISTRADOR"), listarPendientes);
+router.get("/admin/vinculaciones", autenticar, autorizar("ADMINISTRADOR"), listarVinculacionesActivas);
 router.patch("/admin/:id/revisar", autenticar, autorizar("ADMINISTRADOR"), revisarSolicitud);
+router.patch("/admin/vinculaciones/:id/revocar", autenticar, autorizar("ADMINISTRADOR"), revocarVinculacion);
 
 module.exports = router;
