ceb8709d7a674f0f63b5b8f12f3e167998f8a0d8
Jairo Matías <jairo_1829@hotmail.com>
2026-08-19 19:48:57 -0400

    style: load verification badge styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,6 +14,7 @@ import "./styles/talent-dashboard.css";
 import "./styles/user-dashboard.css";
 import "./styles/workspace.css";
 import "./styles/actor-profile.css";
+import "./styles/verification-badges.css";
 
 import App from "./App.jsx";
 
