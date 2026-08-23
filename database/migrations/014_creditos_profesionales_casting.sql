9ccab7e74d2d3530f58e76598cda14ab461ff919
Jairo Matías <jairo_1829@hotmail.com>
2026-08-23 10:19:02 -0400

    feat: extend casting credit type across claims

--- database/migrations/014_creditos_profesionales_casting.sql
+++ database/migrations/014_creditos_profesionales_casting.sql
@@ -23,3 +23,26 @@ ALTER TABLE dbo.PeliculaCreditos ADD CONSTRAINT CK_PeliculaCreditos_Tipo CHECK
     )
 );
 GO
+
+IF OBJECT_ID(N'dbo.CK_SolicitudesCredito_Tipo', N'C') IS NOT NULL
+BEGIN
+    ALTER TABLE dbo.SolicitudesCredito DROP CONSTRAINT CK_SolicitudesCredito_Tipo;
+END
+GO
+
+ALTER TABLE dbo.SolicitudesCredito ADD CONSTRAINT CK_SolicitudesCredito_Tipo CHECK
+(
+    TipoParticipacion IN
+    (
+        N'ACTOR',
+        N'DIRECTOR',
+        N'DIRECTOR_CASTING',
+        N'PRODUCTOR',
+        N'GUIONISTA',
+        N'COMPOSITOR',
+        N'FOTOGRAFIA',
+        N'EDICION',
+        N'OTRO'
+    )
+);
+GO
